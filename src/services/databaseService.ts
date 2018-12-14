7df8027e4c8087bb4e10e4335c7d287e7dbf475e
AntonyHatchet <antongorshenin@gmail.com>
2018-12-15 01:48:16 +0300

    Забытые файлы

---
+++ src/services/databaseService.ts
@@ -0,0 +1,36 @@
+import {DB} from '../api/db';
+
+export class DbService {
+
+    queryString: string;
+
+    constructor(queryString) {
+        this.queryString = queryString;
+    }
+
+    public async get(limit): Promise<any> {
+        const db = new DB();
+        const response = await db.query(this.queryString);
+        if (response.success) {
+            let data = [];
+            let pageCount = response.data && response.data.length > 0 ? Math.ceil(response.data[0].rows[0].count / limit) : 0;
+
+            let rows = response.data && response.data.length > 1 ? response.data[1].rows : [];
+            for (let row of rows) {
+                let keys = Object.keys(row);
+                let obj = {};
+
+                for (let key of keys) {
+                    obj[key] = row[key];
+                }
+
+                data.push(obj);
+            }
+
+            return {success: true, data: data, pageCount: pageCount};
+        } else {
+            return {success: false, error: response.data};
+        }
+    }
+
+}
